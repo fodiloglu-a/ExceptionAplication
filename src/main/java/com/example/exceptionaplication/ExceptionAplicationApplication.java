5860c31cac7b35b2518c903d22e2bc72b21e4964
fodiloglu-a <mutalip.fodiloglu@gmail.com>
2023-07-03 02:59:29 +0300

    firt commit for base branch

---
+++ src/main/java/com/example/exceptionaplication/ExceptionAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptionaplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionAplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionAplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exceptionaplication/ExceptionAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exceptionaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionAplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
