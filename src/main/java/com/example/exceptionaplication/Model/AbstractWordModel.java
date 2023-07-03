4a7c04d22f2e8433e2f9407a0f791e3ec8453d60
fodiloglu-a <mutalip.fodiloglu@gmail.com>
2023-07-03 15:59:26 +0300

    development:"EnglishWordModel ant outher commit"

--- src/main/java/com/example/exceptionaplication/Model/AbstractWordModel.java
+++ src/main/java/com/example/exceptionaplication/Model/AbstractWordModel.java
@@ -1,14 +1,11 @@
 package com.example.exceptionaplication.Model;
 
-import lombok.Data;
-import lombok.NonNull;
-import org.springframework.data.annotation.Id;
-
-@Data
-public abstract class AbstractWordModel {
+public  class AbstractWordModel {
 
     private Long id;
 
+    private String wordTurkish;
 
+    private String wordEnglish;
 
 }
--- src/main/java/com/example/exceptionaplication/services/EnglishServicesImp.java
+++ src/main/java/com/example/exceptionaplication/services/EnglishServicesImp.java
@@ -1,4 +1,55 @@
 package com.example.exceptionaplication.services;
 
+import com.example.exceptionaplication.Model.EnglishWordModel;
+import com.example.exceptionaplication.dao.EnglishWordDao;
+import com.example.exceptionaplication.mapper.WordDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class EnglishServicesImp implements EnglishServices{
+
+    private final EnglishWordDao englishWordDao;
+    private final ModelMapper modelMapper;
+
+    public EnglishServicesImp(EnglishWordDao englishWordDao, ModelMapper modelMapper) {
+        this.englishWordDao = englishWordDao;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public List<WordDTO> getAll() {
+        List<EnglishWordModel> englishWordModels=englishWordDao.findAll();
+        List<WordDTO> wordDTOS=new ArrayList<>();
+
+        for (EnglishWordModel englishWordModel : englishWordModels) {
+            wordDTOS.add(modelMapper.map(englishWordModel, WordDTO.class));
+
+        }
+
+        return wordDTOS;
+    }
+
+    @Override
+    public List<WordDTO> getByOrLike(String word) {
+        List<EnglishWordModel> englishWordModels=englishWordDao.getEnglishWordModelByWordEnglishLike(word);
+        List<WordDTO> wordDTOS=new ArrayList<>();
+        for (EnglishWordModel englishWordModel : englishWordModels) {
+            wordDTOS.add(modelMapper.map(englishWordModel,WordDTO.class));
+        }
+        return wordDTOS;
+    }
+
+    @Override
+    public WordDTO create(WordDTO wordDTO) {
+        EnglishWordModel englishWordModel=modelMapper.map(wordDTO,EnglishWordModel.class);
+
+        englishWordDao.save(englishWordModel);
+
+
+        return wordDTO;
+    }
 }
