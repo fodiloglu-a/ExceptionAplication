08218e1ef59bf60695a56b5791cd9bb02ef9553f
fodiloglu-a <mutalip.fodiloglu@gmail.com>
2023-07-14 00:10:37 +0300

    Finally commit for exceptionAplications

--- Readme.md
+++ Readme.md
@@ -10,3 +10,48 @@
 (agg, apple, rad, big, small) and you don't want to forget these 
 words. In this case, you can add them to the dictionary app and 
 keep track of them.
+
+
+## ExceptionHandling Nedir?
+
+- Exception: Kelime anlamı olarak hata anlamına gelmektedir.Temel amaç sistem de oluşan hataları bilmek ve yönetmektir.
+- Sistem de çıkan hatalar incelendiği zaman belirli başlıklar altında toplanılabilmektedir.
+- Peki toplanan bu kadar hatanın bizee faydası nedir?Bu soruya aslında tek bir cümlede cevap vermek
+gerekirse alınan hatalardan anlamlı sonuçlar çıklarıp hem kullanıcı deneyimini artırıp hem de sistemimizin verimliliğini artırmaktır.
+
+## Exception Türleri
+
+- Aslında exception türleri çok fazladır ve programımıza göre de farklılık göstere bilir.
+- Java da Exception class incelendiğinde Throwableclasından extend edildiği görülmektedir.Bu class
+incelendiği zaman yaklaşık 16** farklı class bu classdan extend edilmiştir.
+
+
+## Handle etmek ne demektir
+
+- Handle etmek aslında üstesinden gelmek olarak yorumlanabilir. Alınan hatanın üstesinden gelmek
+kısacası hatayı anlaşılır kılmak olarak tanımlanabilir.
+- Nasıl diye soracak olursak örneğin bir hata fırlatıyor sistemimiz ve hatanın içeriği şu şekilde
+{"NOT BE NULL"} peki bu hata da anşadığımız nedir bir şeyin null olmaması ama o şey ne diye soracak olursak
+bilinmez bu hatanın handle dilmiş hali şöyle oluyor.{Customer name not be null} diye tanımlarsak yani
+handl edersek dah anlamlı sonuçlar alacağız.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/example/exceptionaplication/exceptio/ExceptionsHandler.java
+++ src/main/java/com/example/exceptionaplication/exceptio/ExceptionsHandler.java
@@ -9,28 +9,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionsHandler {
 
     @ExceptionHandler(CustomerException.class)
-    public ResponseEntity customerException(CustomerException customerException){
+    public ResponseEntity<?> customerException(CustomerException customerException){
         ErrorResponse errorResponse=new ErrorResponse();
         errorResponse.setMessage(customerException.message);
         errorResponse.getDetails().add(errorResponse.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(WordNotFountException.class)
-    public ResponseEntity wordNotFountException(WordNotFountException wordNotFountException){
+    public ResponseEntity<?> wordNotFountException(WordNotFountException wordNotFountException){
         ErrorResponse errorResponse=new ErrorResponse();
         errorResponse.setMessage(wordNotFountException.getMessage());
         errorResponse.getDetails().add(errorResponse.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler(WordNotSaveException.class)
-    public ResponseEntity wordNotSaveException(WordNotSaveException wordNotSaveException){
+    public ResponseEntity<?> wordNotSaveException(WordNotSaveException wordNotSaveException){
         ErrorResponse errorResponse=new ErrorResponse();
         errorResponse.setMessage(wordNotSaveException.getMessage());
         errorResponse.getDetails().add(errorResponse.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.CREATED);
     }
     @ExceptionHandler(WordNotBeNullException.class)
-    public ResponseEntity wordNotBeNullException(WordNotBeNullException wordNotBeNullException){
+    public ResponseEntity<?> wordNotBeNullException(WordNotBeNullException wordNotBeNullException){
         ErrorResponse errorResponse=new ErrorResponse();
         errorResponse.setMessage(wordNotBeNullException.getMessage());
         errorResponse.getDetails().add(errorResponse.getMessage());
